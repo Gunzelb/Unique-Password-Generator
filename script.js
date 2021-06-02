eae6e8e5b7496f6eb3bce25d216227894f425deb
Gunzelb <brandongunzel@gmail.com>
2021-06-01 18:12:30 -0700

    further editing of script.js. added comments

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+//character arrays for use by the writePassword function
 const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const special = ["\u0021", "\u0023", "\u0024", "\u0025", "\u0026", "\u002B", "\u002D", "\u003C", "\u003D", "\u003E", "\u003F", "\u0040", "\u005E", "\u005F", "\u007E"];
+
+function swap(x, y) {
+  let temp = x;
+  x = y;
+  y = temp;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -16,7 +22,7 @@ function writePassword() {
     var characters = [];
     var pass = [];
     var passLength = prompt("Enter password length between 8 and 128.");
-
+    //checks if the user entered a valid password length and gathers information on what character types the user would like to use.
     if (passLength != null && passLength >= 8 && passLength <= 128) {
       alert("Please select OK to use the following character types in your password.");
       var lowerCase = confirm("Lowercase?");
@@ -27,6 +33,7 @@ function writePassword() {
         alert("Invalid selection: Please select at least 1 character type.");
         return
       }
+      //adds 2 characters of each type specified by the user. will add at most 8 characters to the password which is the minimum password length.
       var s = 2;
       if (lowerCase) {
         characters.push(lower);
@@ -44,7 +51,7 @@ function writePassword() {
         characters.push(number);
         for (k = 0; k < s; k++) {
           pass.push(number[(Math.floor(Math.random() * (number.length - 1)))])
-        }  
+        }
       }
       if (specialC) {
         characters.push(special);
@@ -52,33 +59,26 @@ function writePassword() {
           pass.push(special[(Math.floor(Math.random() * (special.length - 1)))])
         }
       }
-      console.log(pass);
-      }
-
-      function swap(x,y) {
-        let temp = x;
-        x = y;
-        y = temp;
-      }
-
-      for (l = 0; l < (Math.floor(Math.random() * (pass.length - 1))); l++) {
-        let m = (Math.floor(Math.random() * (pass.length - 1)))
-        swap(pass[l], pass[m]);
-      }
-
-      if (passLength === pass.length) {
-        return pass.join('');
-      }
-      else {
-        for (i = 0; i < (passLength - pass.length); i++) {
-          pass.push(characters[(Math.floor(Math.random() * (characters.length - 1)))]);
-        }
-        return pass.join('');
-      }
-      
     }
 
+
+    //for (l = 0; l < (Math.floor(Math.random() * (pass.length - 1))); l++) {
+      // let m = (Math.floor(Math.random() * (pass.length - 1)))
+      // swap(pass[l], pass[m]);
+      // debugger;
+    // }
+    // adds additional random characters to password if needed per user specifications 
+    if (passLength != pass.length) {
+      for (i = 0; i < (passLength - pass.length); i++) {
+        let chars = characters[(Math.floor(Math.random() * (characters.length - 1)))]
+        pass.push(chars[(Math.floor(Math.random() * (chars.length - 1)))]);
+      }
+    }
+    console.log(pass);      
+    return pass.join('');
   }
+
+}
 
 
 // Add event listener to generate button
